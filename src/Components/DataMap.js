ba52d05f4715ab4b6e029ff8cdbf1de5373f9d4f
Michael <delcambrem@gmail.com>
2022-01-04 16:59:27 -0600

    4:59pm

--- src/Components/DataMap.js
+++ src/Components/DataMap.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import RestaurantList from './RestaurantList';
+// import RestaurantList from './RestaurantList';
+import { Link, useNavigate } from "react-router-dom"
 
 
 function DataMap ({ data }) {
 
+        const navigate = useNavigate()
 
     return (
         <div>
-            {
-                data.map(d => ( 
-                                
-                    <RestaurantList key={d.id}
-                                        restaurant={d.restaurant}
-                                        time={d.time} 
-                                        />
-                                
-                ))
-            }
+            <nav>
+                <select onChange={() => navigate('./Form')}>
+                    <option value="which-restaurant">Where do you wanna eat?</option>
+                    {data.map(d => ( 
+                        <option key={d.id}>{d.restaurant}</option>
+                    ))}
+                </select>
+            </nav>
         </div>
     )
-
 }
 
 
