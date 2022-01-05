08201c3d59cfe59d92a790b0e8ae5fb1e393b926
Michael <delcambrem@gmail.com>
2022-01-04 18:39:32 -0600

    6:39

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'
 
 export default function Header () {
 
-    const navigate = useNavigate()
     
     return (
         (
--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
-import React from 'react';
-// import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react';import './index.css';
 import App from './Components/App';
 import Form from './Components/Form'
 import DataMap from './Components/DataMap'
 import ConfirmReservation from './Components/ConfirmReservation'
-import reportWebVitals from './reportWebVitals';
 import { render } from "react-dom"
 import { BrowserRouter, 
   Routes, 
   Route
 } from "react-router-dom";
 
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 const rootElement = document.getElementById("root");
 render(
@@ -32,8 +22,3 @@ render(
   </BrowserRouter>, 
   rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
